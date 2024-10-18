8b7d73c47fe256b24f08aa525d0f8f25585600be
Jorge Guzman <jaguzman3@miuandes.cl>
2024-10-17 21:18:38 -0300

    course CRUD

--- assignment2/MyApp2/app/controllers/courses_controller.rb
+++ assignment2/MyApp2/app/controllers/courses_controller.rb
@@ -1,7 +1,49 @@
 class CoursesController < ApplicationController
     
-    def index
-      @courses = Course.all
+  def index
+    @courses = Course.all
+  end
+
+  def show
+    @course = Course.find params['id']
+  end
+
+  def new
+    @course = Course.new
+  end
+
+  def create
+    course = Course.new course_params
+    if course.save
+      redirect_to course_path(course)
+    else
+      redirect_to new_course_path
     end
   end
+
+  def edit
+    @course = Course.find params['id']
+  end
+
+  def updated
+    course = Course.find params['id']
+    if course.update course_params
+      redirect_to edit_course_path(course)
+    else
+      redirect_to courses_path, alert: "libro no se pudo actualizar"
+    end
+  end
+
+  def destroy
+    course = Course.find params['id']
+    course.destroy
+    redirect_to courses_path
+  end
+
+  private 
+  def course_params
+    params.require(:course).permit(:title, :description)
+  end
+
+end
   
